3c8b78065f5b661f10fca27a2c8472b798226e81
Andrea <andreademarco91@gmail.com>
2020-02-13 16:00:17 +0100

    PRIME FUNZIONI

--- js/script.js
+++ js/script.js
@@ -1,1 +1,25 @@
-alert ('ciao')
+$(document).ready(function(){
+  var source = document.getElementById("entry-template").innerHTML;
+  var template = Handlebars.compile(source);
+  $.ajax({
+    url: "http://157.230.17.132:3034/todos",
+    method: "GET",
+    success: function (data, stato) {
+      for (var i = 0; i < data.length; i++) {
+        var element = data[i].text
+        var context = {
+          'element': element,
+          };
+        console.log(context);
+        var html = template(context);
+        $('.list').append(html)
+      }
+    },
+    error: function (richiesta, stato, errori) {
+    alert("E' avvenuto un errore. " + errore);
+    }
+  });
+  $(document).on("click", ".delete", function() {
+    alert ('cangelli')
+  });
+})
